a4cb05f494d900e50e60018a53579d60e0fcf465
Bertrand Bonnefoy-Claudet <bertrand@bertrandbc.com>
2023-09-02 19:50:22 +0200

    Rename test files
    They now all have the .lua extension, for easier editing in text editors.

--- test/fmt/gen/gen.ml
+++ test/fmt/gen/gen.ml
@@ -1,16 +1,16 @@
 let extract_name file =
-  file |> Str.full_split (Str.regexp {|\.lua$|}) |> function
+  file |> Str.full_split (Str.regexp {|\.in\.lua$|}) |> function
   | [Str.Text name; Delim _] -> Some name
   | _ -> None
 
 let gen_case name =
   Printf.sprintf
     {|(rule
- (with-stdout-to %s.out (run %%{bin:luafmt} %%{dep:cases/%s.lua})))
+ (with-stdout-to %s.out.lua (run %%{bin:luafmt} %%{dep:cases/%s.in.lua})))
 
 (rule
  (alias runtest)
- (action (diff cases/%s.exp %s.out)))|}
+ (action (diff cases/%s.exp.lua %s.out.lua)))|}
     name name name name
 
 let () =
